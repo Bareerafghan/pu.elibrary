a64117aeb0997042fd9cbd6f311c69bcc47d0695
Bareerafghan <95598861+Bareerafghan@users.noreply.github.com>
2023-04-09 23:30:22 +0430

    Add files via upload

---
+++ index.php
@@ -0,0 +1,301 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+</head>
+<body class="app sidebar-mini rtl" >
+	<?php require 'General.php'; ?>
+			<!-- Sidebar menu-->
+            <!-- app-content-->
+			<!-- Top navbar -->
+			<!-- User -->		
+		
+						<!-- Page content -->
+<div class="container-fluid pt-8">
+<div class="page-header mt-0 shadow p-3">
+<ol class="breadcrumb mb-sm-0">
+<li class="breadcrumb-item"><a href="index.php">Home</a></li>
+<li class="breadcrumb-item active" aria-current="page">E-Libray contents</li>
+								</ol>
+<div class="btn-group mb-0">
+<button type="button" class="btn btn-primary btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Language</button>
+<div class="dropdown-menu">
+<a class="dropdown-item" href="#"><i class=""></i>English</a>
+<div class="dropdown-divider"></div>
+<a class="dropdown-item" href="#"><i class=""></i>Pashto</a>
+<div class="dropdown-divider"></div>
+<a class="dropdown-item" href="#"><i class=""></i>Dari</a>
+								</div>
+								</div>
+							</div>
+	<div class="card shadow ">
+	<div class="">
+	<div class="row">
+	<div class="col-xl-2 col-lg-4 col-md-6 col-sm-5 container-fluid stats">
+	<div class="text-center">
+	<p class="text-light">Books</p>
+	<h2 class="text-primary text-xxl">1235</h2>
+										</div>
+										</div>
+	<div class="col-xl-2 col-lg-4 col-md-6 col-sm-5 container-fluid stats">
+	<div class="text-center">
+	<p class="text-light"> Monographs</p>
+	<h2 class="text-yellow text-xxl">523</h2>
+												
+										</div>
+										</div>
+	<div class="col-xl-2 col-lg-4 col-md-6 col-sm-5 container-fluid stats">
+	<div class="text-center">
+	<p class="text-light">  Local Articals</p>
+	<h2 class="text-warning text-xxl">785</h2>
+												
+										</div>
+										</div>
+	<div class="col-xl-2 col-lg-4 col-md-6 col-sm-5 container-fluid stats">
+	<div class="text-center">
+	<p class="text-light">  Conferences </p>
+	<h2 class="text-danger text-xxl">125</h2>
+										</div>
+										</div>
+	<div class="col-xl-2 col-lg-4 col-md-6 col-sm-5 container-fluid stats">
+	<div class="text-center">
+	<p class="text-light"> Magazines</p>
+	<h2 class="text-success text-xxl"> 300</h2>
+												
+									</div>
+									</div>
+									</div>
+								</div>
+							</div>
+    <!-- ------------- Propessors--------------->
+    <div class="row">
+		<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 container-fluid stats">
+		<div class="card shadow">
+		<div class="card-header bg-transparent">
+		<div class="row align-items-center">
+		<div class="col">
+		<h2 class="mb-0">Propessors</h2>
+		<h6 class="text-uppercase text-light ls-1 mb-1">List of Propessors who uptade uploaded</h6>
+		
+										</div>
+										</div>
+										</div>
+	<div class="card shadow overflow-hidden">
+	<div class="">
+	<div class="row">
+	<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 container-fluid stats">
+	<div class="card-body text-center">
+	<img class="avatar avatar-xxl brround" src="Image/bareer.jpg" alt="avatra-img">
+	<h2 class="h3 mb-0 mt-3 font-600">Ass. Pro. Ahmad.................</h2>
+	<p class="card-text text-sm">Web-based E-Library plateform Evaluation </p>
+	</div>
+										</div>
+	<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 container-fluid stats">
+	<div class="card-body text-center">
+	<img class="avatar avatar-xxl brround" src="Image/ba.jpg" alt="avatra-img">
+	<h2 class="h3 mb-0 mt-3 font-600">Ass. Pro. Ahmad.................</h2>
+	<p class="card-text text-sm">Web designer</p>
+	</div>
+	</div>
+	<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 container-fluid stats">
+	<div class="card-body text-center">
+	<img class="avatar avatar-xxl brround" src="Image/logo_paktia.png" alt="avatra-img">
+	<h2 class="h3 mb-0 mt-3 font-600">Ass. Pro. Ahmad.................</h2>
+	<p class="card-text text-sm">Web designer</p>
+	</div>
+    </div>
+	<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 container-fluid stats">
+	<div class="card-body text-center">
+	<img class="avatar avatar-xxl brround" src="Image/map.png" alt="avatra-img">
+	<h2 class="h3 mb-0 mt-3 font-600">Ass. Pro. Ahmad.................</h2>
+	<p class="card-text text-sm">Web designer</p>
+	</div>
+    </div>
+							</div>
+							</div>
+							</div>
+			
+			<!-- ------------ Chart ------------ -->
+		
+		
+		<div class="row">
+		<div class="col-xl-6 container-fluid">
+		<div class="card-body ">
+		<div class="card-header">
+		<h2 class="mb-0">Leave us your Suggetions</h2>
+		</div>
+		<div class="card-body ">
+        <div class="form-group">
+		
+		<input type="text" class="form-control" name="example-text-input" placeholder="Enter Your Name">
+		</div>
+		       <div class="form-group">
+		
+		<input type="text" class="form-control" name="example-text-input" placeholder="Enter Yuor Email">
+		</div>
+        <div class="form-group">
+		
+		<select name="Jobs" class="form-control custom-select" >
+		<option value="" selected disabled > Chose your Occupation </option>
+		<option value="Teacher" > Teacher </option>
+		<option value="Student" >Student</option>
+		</select>
+		</div>
+		             
+		<div class="form-group ">
+		<textarea class="form-control" name="example-textarea-input" rows="2" placeholder="Write your Suggetions here.."></textarea>
+		</div>
+        <div class="form-group ">
+	<a href="#" class="btn btn-primary  mb-0 form-control"> Send </a>
+	</div>
+		</div> </div> </div> 
+
+
+		<div class="col-xl-6 ">
+			<div class="card-body ">
+<div class="card-header">
+		<h2 class="mb-0">Available Resource Per %</h2>
+		</div>
+			
+		<div class="progress-wrapper pt-2">
+		<div class="progress-info">
+		<div class="progress-label">
+		<span>Books</span>
+		</div>
+		<div class="progress-percentage">
+		<span>70%</span>
+		</div>
+		</div>
+		<div class="progress">
+		<div class="progress-bar bg-primary" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%;"></div>
+		</div>
+		</div>
+		<div class="progress-wrapper pt-2">
+		<div class="progress-info">
+		<div class="progress-label">
+		<span>Monographs</span>
+		</div>
+		<div class="progress-percentage">
+		<span>45%</span>
+		</div>
+		</div>
+		<div class="progress">
+		<div class="progress-bar bg-warning" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 45%;"></div>
+		</div>
+		</div>
+		<div class="progress-wrapper pt-2">
+		<div class="progress-info">
+		<div class="progress-label">
+		<span>Local Articles</span>
+		</div>
+		<div class="progress-percentage">
+		<span>60%</span>
+		</div>
+		</div>
+		<div class="progress">
+		<div class="progress-bar bg-info" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;"></div>
+		</div>
+		</div>
+		<div class="progress-wrapper pt-2">
+		<div class="progress-info">
+		<div class="progress-label">
+		<span>Internation Articales</span>
+		</div>
+		<div class="progress-percentage">
+		<span>56%</span>
+		</div>
+		</div>
+		<div class="progress">
+		<div class="progress-bar bg-success" role="progressbar" aria-valuenow="56" aria-valuemin="0" aria-valuemax="100" style="width: 56%;"></div>
+		</div>
+		</div>
+		<div class="progress-wrapper pt-2">
+		<div class="progress-info">
+		<div class="progress-label">
+		<span>Magazines</span>
+		</div>
+		<div class="progress-percentage">
+		<span>30%</span>
+		</div>
+		</div>
+		<div class="progress">
+		<div class="progress-bar bg-danger" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: 30%;"></div>
+		</div>
+		</div>
+		<div class="progress-wrapper pt-2">
+		<div class="progress-info">
+		<div class="progress-label">
+		<span>Conferences</span>
+		</div>
+		<div class="progress-percentage">
+		<span>30%</span>
+		</div>
+		</div>
+		<div class="progress">
+		<div class="progress-bar bg-warning" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: 30%;"></div>
+	</div> 
+</div>
+							</div>
+							</div>
+							</div>
+							
+<!--  Conferences ..........................-->
+					<div class="row">
+					<div class="col">
+					<div class="card shadow">
+					<div class="card-header bg-transparent border-0">
+					<h2 class=" mb-0">List Of Conferences</h2>
+										</div>
+					<div class="">
+					<div class="grid-margin">
+					<div class="">
+					<div class="table-responsive">
+	<table class="table card-table table-vcenter text-nowrap align-items-center">
+	<thead class="thead-light">
+	<tr>
+	<th>Topic</th>
+	<th>Presenter</th>
+	<th>Date</th>
+	<th>Time</th>
+	<th>Location</th>
+	</tr>
+	</thead>
+	<tbody>
+	<tr>
+	<td class="text-sm font-weight-600">Web-based E-library Platform evaluation </td>
+	<td>Tea. Ass. B................</td>
+	<td>2024-4-25</td>
+	<td> 15:30</td>
+	<td> Education faculty PCD</td>			
+	</tr>
+	</tbody>
+	</table>
+			</div>
+				</div>
+					</div>
+						</div>
+							</div>
+							   </div>
+							      </div>
+						<!-- Footer -->
+		<footer class="footer">
+		<div class="row align-items-center justify-content-xl-between">
+		<div style="width: 100%;">
+		<div class="copyright text-center text-xl-left text-muted">
+		<p class="text-sm font-weight-500 text-center">Copyright 2024 © All Rights Reserved.</p>
+										</div>
+			</div>
+		    </div>
+			</footer>
+							<!-- Footer -->
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- app-content-->
+		</div>
+	</div>
+	
+</body>
+
+</html>
